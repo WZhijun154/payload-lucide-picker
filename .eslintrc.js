30094e6055ab7e6cac56b87bdd471525b1cb5e67
wangzhijun <wangzhijun154@gmail.com>
2025-03-08 09:03:44 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,31 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'prettier',
+    'prettier'
   ],
   plugins: ['@typescript-eslint', 'react', 'prettier'],
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
   rules: {
-    'prettier/prettier': 'off',
+    'prettier/prettier': 'warn',
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    'quotes': 'off',
-    'eol-last': 'off',
-  },
+    '@typescript-eslint/no-unused-vars': 'warn'
+  }
 }; 
