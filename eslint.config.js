9b42e1ba8b36e4496e81846000665bbac65b4107
wangzhijun <wangzhijun154@gmail.com>
2025-03-08 09:07:28 +0800

    update

--- eslint.config.js
+++ eslint.config.js
@@ -3,15 +3,17 @@ import tsPlugin from '@typescript-eslint/eslint-plugin';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import prettierPlugin from 'eslint-plugin-prettier';
+import prettier from 'prettier';
 
 export default [
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['dist/**', 'node_modules/**'],
     languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       parser: tsParser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
         ecmaFeatures: {
           jsx: true
         }
@@ -29,7 +31,7 @@ export default [
       }
     },
     rules: {
-      'prettier/prettier': 'warn',
+      'prettier/prettier': ['warn', await prettier.resolveConfig('.') ?? {}],
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
